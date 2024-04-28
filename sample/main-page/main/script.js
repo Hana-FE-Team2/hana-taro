f958b8e85c9920174e30f6d1989407ba6ba47a4a
moho852 <moho191113@gmail.com>
2024-04-28 16:57:01 +0900

    feat: hanatort 01 작업 중간 커밋

--- sample/main-page/main/script.js
+++ sample/main-page/main/script.js
@@ -1,11 +1,15 @@
+// magicball js
 const magicBall = document.querySelector(".magicball1")
+// magicball에 마우스 올렸을 때
 magicBall.addEventListener("mouseover", ()=>{
   magicBall.classList.add("magicball-hover")
 })
+// magicball에 마우스 나갔을 때
 magicBall.addEventListener("mouseout", () => {
   magicBall.classList.remove("magicball-hover")
 })
 
+// message 띄우려고 가져옴
 let nextButtonClickCount = 0;
 function openModal(messageList) {
 const modal = $('#modal');
@@ -26,13 +30,14 @@ const modal = $('#modal');
 const cards = document.querySelectorAll("#cards")
 const card = document.querySelectorAll(".card")
 const background = document.querySelector(".backgroud")
-// const clickedCard = document.querySelectorAll("#cards.clicked")
 cards.forEach((ele) => {
   console.log(ele)
+  // 카드 클릭 시 
   ele.addEventListener('click', () => {
     const clickedCard = document.querySelectorAll("#cards.clicked")
     ele.classList.contains("clicked") ?  ele.classList.remove("clicked") : ele.classList.add("clicked")
     console.log(document.querySelectorAll("#cards.clicked").length)
+    // 카드를 4장 째 뒤집을 시
     if (clickedCard.length+1 >= 4) {
       clickedCard.forEach((elem)=> elem.classList.remove("clicked"))
       document.querySelector("#cards.clicked").classList.remove("clicked")
@@ -41,14 +46,3 @@ cards.forEach((ele) => {
     }
   })
 })
-
-// mouse-movement
-// const background = document.querySelector(".backgroud")
-// background.addEventListener('click', () => {
-//   background.classList.contains("clicked") ? background.classList.remove("clicked") : background.classList.add("clicked") 
-// })
-
-// 모달 열기 버튼 클릭 시
-// $('#openModal').on('click', function () {
-//   background.classList.contains("clicked") ? background.classList.remove("clicked") : background.classList.add("clicked")
-// });
